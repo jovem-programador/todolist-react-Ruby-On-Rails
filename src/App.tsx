026c58513280a10607d476a1f711f6f3efb8bcd4
Anderson Marley <andersonmarley011@gmail.com>
2024-01-30 23:09:24 -0300

    Configuração

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import TaskManager from "./components/task_manager";
 import './App.css'
+import { FC } from "react";
 
-function App() {
-
-  return (<TaskManager></TaskManager>)
-}
+const App: FC = () => <TaskManager></TaskManager>
 
 export default App
